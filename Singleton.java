4fa6cdcd0c5e29f5260e54510b96b58b20b668d9
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-07 22:30:21 -0500

    Add files via upload

--- Singleton.java
+++ Singleton.java
@@ -1,2 +1,9 @@
 public class Singleton {
+    public static Singleton instance = new Singleton();
+    public static Singleton getInstance(){
+        return instance;
+    }
+    private Singleton(){
+
+    }
 }
